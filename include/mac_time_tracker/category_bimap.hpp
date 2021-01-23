4ff73a3c258b5ead5abf559f142231d80a372db2
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2021-01-23 17:25:16 +0900

    use io helper in CategoryBimap

--- include/mac_time_tracker/category_bimap.hpp
+++ include/mac_time_tracker/category_bimap.hpp
@@ -1,6 +1,7 @@
 #ifndef MAC_TIME_TRACKER_CATEGORY_BIMAP_HPP
 #define MAC_TIME_TRACKER_CATEGORY_BIMAP_HPP
 
+#include <iostream>
 #include <stdexcept>
 #include <string>
 
@@ -12,6 +13,7 @@
 
 #include <mac_time_tracker/address.hpp>
 #include <mac_time_tracker/csv.hpp>
+#include <mac_time_tracker/io.hpp>
 
 namespace mac_time_tracker {
 
@@ -34,7 +36,7 @@ struct CategoryBimapTraits {
   using Base = boost::bimaps::bimap<CategoryMultiset, AddressSet, WithDescription>;
 };
 
-class CategoryBimap : public CategoryBimapTraits::Base {
+class CategoryBimap : public CategoryBimapTraits::Base, public Readable<CategoryBimap> {
 private:
   using Base = CategoryBimapTraits::Base;
 
@@ -47,20 +49,19 @@ public:
   CategoryBimap(const Base &base) : Base(base) {}
   CategoryBimap(Base &&base) : Base(base) {}
 
-  // Create an instance from a file like
+  // Create an instance from a CSV like
   //     0: <c[0]>, <a[0][0]>, <d[0][0]>, ..., <d[0][i]>, <d[0][i]>
   //   ...
   //     k: <c[k]>, <a[k][0]>, <d[k][0]>, ..., <d[k][j]>, <d[k][j]>
   // where c[n]    -> n-th category,
   //       a[n][m] -> m-th MAC address in n-th category
   //       d[n][m] -> description of a[n][m]
-  static CategoryBimap fromFile(const std::string &filename) {
+  static CategoryBimap fromCSV(const CSV &csv) {
     CategoryBimap map;
-    for (const std::vector<std::string> &line : CSV::fromFile(filename)) {
+    for (const std::vector<std::string> &line : csv) {
       if (line.size() < 3 || line.size() % 2 == 0) {
-        throw std::runtime_error("CategoryBimap::fromFile(): Invalid number of elements (" +
-                                 boost::lexical_cast<std::string>(line.size()) +
-                                 ") in a line of '" + filename + "'");
+        throw std::runtime_error("CategoryBimap::fromCSV(): Invalid number of elements (" +
+                                 boost::lexical_cast<std::string>(line.size()) + ")");
       }
       // boost::trim_copy() removes leading and trailing spaces
       const std::string category = boost::trim_copy(line[0]);
@@ -68,13 +69,24 @@ public:
         const Address addr = Address::fromStr(boost::trim_copy(line[i]));
         const std::string desc = boost::trim_copy(line[i + 1]);
         if (!map.insert({category, addr, desc}).second) {
-          throw std::runtime_error("CategoryBimap::fromFile(): Cannot insert an item {'" +
-                                   category + "', " + addr.toStr() + ", '" + desc + "'} from '" +
-                                   filename + "'. Non-unique MAC address?");
+          throw std::runtime_error("CategoryBimap::fromCSV(): Cannot insert an item {'" + category +
+                                   "', " + addr.toStr() + ", '" + desc +
+                                   "'}. Non-unique MAC address?");
         }
       }
     }
     return map;
+  }
+
+private:
+  virtual void read(std::istream &is) override {
+    CSV csv;
+    is >> csv;
+    try {
+      *this = fromCSV(csv);
+    } catch (const std::runtime_error &) {
+      is.setstate(std::istream::failbit);
+    }
   }
 };
 } // namespace mac_time_tracker
