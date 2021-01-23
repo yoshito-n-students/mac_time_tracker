c6a8907153f98e276a07873edee86b4b7ed2e189
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2021-01-23 16:35:25 +0900

    use io helper in Time

--- include/mac_time_tracker/time.hpp
+++ include/mac_time_tracker/time.hpp
@@ -4,13 +4,16 @@
 #include <chrono>
 #include <ctime>   // for std::localtime()
 #include <iomanip> // for std::put_time()
+#include <iostream>
 #include <string>
 
 #include <boost/lexical_cast.hpp>
 
+#include <mac_time_tracker/io.hpp>
+
 namespace mac_time_tracker {
 
-class Time : public std::chrono::system_clock::time_point {
+class Time : public std::chrono::system_clock::time_point, public Writable {
 private:
   using Base = std::chrono::system_clock::time_point;
 
@@ -26,10 +29,16 @@ public:
   // A shortcut to Time::clock::now()
   static Time now() { return clock::now(); }
 
-  // Convert to string. The default format is based on ISO 8601 ("Y-M-D H:M:S")
-  std::string toStr(const std::string &fmt = "%F %T") const {
+  using Writable::toStr;
+  std::string toStr(const std::string &fmt) const {
     const std::time_t t = clock::to_time_t(*this);
     return boost::lexical_cast<std::string>(std::put_time(std::localtime(&t), fmt.c_str()));
+  }
+
+private:
+  virtual void write(std::ostream &os) const override {
+    // use a format like "Y-M-D H:M:S", which is based on ISO 8601
+    os << toStr("%F %T");
   }
 };
 
